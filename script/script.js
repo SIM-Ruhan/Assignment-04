c3b3923e41161058547e1ebde3622f5b85347b46
SIM Ruhan <simruhan22@gmail.com>
2026-02-23 21:36:35 +0600

    width removed from no job div

--- script/script.js
+++ script/script.js
@@ -135,7 +135,7 @@ function renderTotal(){
     if(allCardSection.children.length === 0){
 
     allCardSection.innerHTML = `
-        <div class="text-center my-10 w-[80%] shadow mx-auto py-10 sm:py-28">
+        <div class="text-center my-10 shadow mx-auto py-10 sm:py-28">
             <i class="fa-solid fa-file-lines text-8xl text-[#7DA8FF]"></i>
             <p class="mt-4 text-2xl font-semibold leading-8">No jobs available</p>
             <p class="text-gray-500">Check back soon for new job opportunities</p>
@@ -148,7 +148,7 @@ function renderTotal(){
 function renderInterview(){
     if(interviewList.length === 0){
     filteredSection.innerHTML = `
-       <div class="text-center my-10 w-[80%] shadow mx-auto py-10 sm:py-28">
+       <div class="text-center my-10 shadow mx-auto py-10 sm:py-28">
             <i class="fa-solid fa-file-lines text-8xl text-[#7DA8FF]"></i>
             <p class="mt-4 text-2xl font-semibold leading-8">No jobs available</p>
             <p class="text-gray-500">Check back soon for new job opportunities</p>
@@ -194,7 +194,7 @@ filteredSection.appendChild(div);
 function renderRejected(){
     if(rejectedList.length === 0){
     filteredSection.innerHTML = `
-        <div class="text-center my-10 w-[80%] shadow mx-auto py-10 sm:py-28">
+        <div class="text-center my-10 shadow mx-auto py-10 sm:py-28">
             <i class="fa-solid fa-file-lines text-8xl text-[#7DA8FF]"></i>
             <p class="mt-4 text-2xl font-semibold leading-8">No jobs available</p>
             <p class="text-gray-500">Check back soon for new job opportunities</p>
