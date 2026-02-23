de2af4f6b81d1598d38aad465f0968644b6274ec
SIM Ruhan <simruhan22@gmail.com>
2026-02-23 21:22:58 +0600

    width adjusted

--- script/script.js
+++ script/script.js
@@ -19,7 +19,8 @@ function calculateCount(){
     total.innerText = allCardSection.children.length;
   interview.innerText = interviewList.length;
   rejected.innerText = rejectedList.length;
-     jobTotal.innerText = allCardSection.children.length;
+jobTotal.innerText = allCardSection.children.length;
+
 }
 calculateCount();
 
@@ -134,7 +135,7 @@ function renderTotal(){
     if(allCardSection.children.length === 0){
 
     allCardSection.innerHTML = `
-        <div class="text-center my-10 w-[70%] shadow mx-auto py-10 sm:py-28">
+        <div class="text-center my-10 w-[80%] shadow mx-auto py-10 sm:py-28">
             <i class="fa-solid fa-file-lines text-8xl text-[#7DA8FF]"></i>
             <p class="mt-4 text-2xl font-semibold leading-8">No jobs available</p>
             <p class="text-gray-500">Check back soon for new job opportunities</p>
@@ -147,7 +148,7 @@ function renderTotal(){
 function renderInterview(){
     if(interviewList.length === 0){
     filteredSection.innerHTML = `
-       <div class="text-center my-10 w-[70%] shadow mx-auto py-10 sm:py-28">
+       <div class="text-center my-10 w-[80%] shadow mx-auto py-10 sm:py-28">
             <i class="fa-solid fa-file-lines text-8xl text-[#7DA8FF]"></i>
             <p class="mt-4 text-2xl font-semibold leading-8">No jobs available</p>
             <p class="text-gray-500">Check back soon for new job opportunities</p>
@@ -193,7 +194,7 @@ filteredSection.appendChild(div);
 function renderRejected(){
     if(rejectedList.length === 0){
     filteredSection.innerHTML = `
-        <div class="text-center my-10 w-[70%] shadow mx-auto py-10 sm:py-28">
+        <div class="text-center my-10 w-[80%] shadow mx-auto py-10 sm:py-28">
             <i class="fa-solid fa-file-lines text-8xl text-[#7DA8FF]"></i>
             <p class="mt-4 text-2xl font-semibold leading-8">No jobs available</p>
             <p class="text-gray-500">Check back soon for new job opportunities</p>
